dc304ff3650b41c0ce9c70f95fc610941cca852f
Tasfiq97 <tasfiqulazad200@gmail.com>
2021-11-22 14:38:43 +0600

    final commit

--- src/components/HeroDetail/HeroDetail.js
+++ src/components/HeroDetail/HeroDetail.js
@@ -6,7 +6,7 @@ const HeroDetail = () => {
     return (
         <div className="hero-detail grid grid-cols-2">
             <div className="flex justify-center flex-col items-center p-7 text-center">
-            <p className="text-xl">Everything is For you</p>
+            <p className="text-xl">Everything is For you only</p>
             <h1 className="text-7xl mb-4">All <span className="text-white font-bold">Life-Skills</span> </h1>
            <h4 className="font-bold text-1xl pr-8 mt-3">Information, Not That Other Crap. Encourage the tutee to initiate and identify the focus of the session and Discuss with the tutee the amount of time necessary to complete each part of their task.</h4>
         
